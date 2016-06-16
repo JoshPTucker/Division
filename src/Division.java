d055847b972d8e5b972a65f7bacdf09402272eeb
oracle <oracle@GBTC441R11RH.montgomerycollege.edu>
2016-06-16 15:32:07 -0400

    Initial commit

---
+++ src/Division.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+class Division {
+  public static void main(String[] args) {
+ 
+  int a, b, result;
+ 
+  try
+  {
+	  Scanner input = new Scanner(System.in);
+  
+  System.out.println("Input two integers");
+ 
+  a = input.nextInt();
+  b = input.nextInt();
+ 
+  result = a / b;
+ 
+  System.out.println("Result = " + result);
+  }
+  catch(ArithmeticException e ){
+  System.out.print("You Cant divide by 0");
+ }
+  }
+  }
